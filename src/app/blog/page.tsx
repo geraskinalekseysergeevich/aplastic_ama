c7ebbf4396cbf4eb9c7983879f0295ce17b294cc
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2025-05-15 22:40:48 +0300

    add anchor links

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+const BlogPage = () => {
+	return (
+		<div>
+			<h1>Блог</h1>
+		</div>
+	)
+}
+
+export default BlogPage
---
+++ src/app/production/page.tsx
@@ -0,0 +1,16 @@
+const ProductionPage = () => {
+	return (
+		<div>
+			<h1>Production</h1>
+			<p id="objects" style={{ marginTop: '100vh' }}>
+				Скамейка
+			</p>
+			<h2 id="gifts" style={{ marginTop: '100vh' }}>
+				Система мотивации инициативной молодежи
+			</h2>
+			<footer style={{ marginTop: '100vh' }}></footer>
+		</div>
+	)
+}
+
+export default ProductionPage
