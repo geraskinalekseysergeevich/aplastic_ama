39f4823976a05b83b159b082c771d3d4b0639cc0
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2025-05-29 00:03:33 +0300

    fix: images loading

--- src/constants/team.ts
+++ src/constants/team.ts
@@ -3,21 +3,21 @@ import { ImageItem } from '@/components/image-blocks/with-description/ImagesWith
 export const TEAM_MEMBERS: ImageItem[] = [
 	{
 		id: 1,
-		img: '/team-block/image_1.svg',
+		img: '/team-block/image_1.png',
 		date: '05.09.24',
 		text: 'Ора! Основатели проекта Маша и Миша позируют после того, как расклеили плакаты в городе',
 		alt: 'team member 1',
 	},
 	{
 		id: 2,
-		img: '/team-block/image_2.svg',
+		img: '/team-block/image_2.png',
 		date: '25.04.24',
 		text: 'Посмотрите, это инженер Лёня договаривается о поставке сырья',
 		alt: 'team member 2',
 	},
 	{
 		id: 3,
-		img: '/team-block/image_3.svg',
+		img: '/team-block/image_3.png',
 		date: '18.07.24',
 		text: 'Ааи, это наш дизайнер Оля улыбается в\u00A0камеру в нашей фирменной футболке',
 		alt: 'team member 3',
